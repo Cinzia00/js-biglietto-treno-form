6670ede61c12fee262b033a4396bcf55d21ae90b
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-02-01 20:05:03 +0100

    Fix sistemato js

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-console.log('ciao')
+console.log('ciao');
 
-const prezzoAlKm = 0.21
+const prezzoAlKm = 0.21;
 
 // Il prezzo del biglietto è definito in base ai km (0.21 € al km)
-const kmElement = document.querySelector('#form input[type="text"]')
-const prezzoBigliettoBase = prezzoAlKm * kmElement
-let scontoDaApplicare = document.querySelector('#form select[name="sconti"]')
-const btnElement = document.querySelector('input[type="button"]')
-console.log(kmElement, scontoDaApplicare)
 
-//Applicare sconto del 20% per i minorenni
-let scontoMinorenni = prezzoBigliettoBase * 0.2
-//Applicare sconto del 40% per gli over 65
-let scontoOver65 = prezzoBigliettoBase * 0.4
+const btnElement = document.getElementById('btn');
 
 btnElement.addEventListener('click', function () {
-    console.log('applica modifiche al box')
-    let sconti = scontoDaApplicare.value
+    console.log(btnElement.value);
+    let scontoDaApplicare = document.getElementById('sconti').value; 
+    console.log('scontoDaApplicare', scontoDaApplicare);
+    let sconti = 0;
+    const kmElement = document.querySelector('#form input[type="text"]').value;
+    console.log('kmElement', kmElement);
+    const prezzoBigliettoBase = prezzoAlKm * parseFloat(kmElement);
+    console.log('prezzoBigliettoBase', prezzoBigliettoBase);
+    //Applicare sconto del 20% per i minorenni
+    let scontoMinorenni = prezzoBigliettoBase * 0.2;
+    console.log('scontoMinorenni', scontoMinorenni);
+    //Applicare sconto del 40% per gli over 65
+    let scontoOver65 = prezzoBigliettoBase * 0.4;
+    console.log('scontoOver65', scontoOver65);
+    console.log(kmElement);
+    
+    switch (scontoDaApplicare) {
+        case '1':
+            sconti = scontoMinorenni
+            break
+        case '2':
+            sconti = scontoOver65
+            break
+        }
 
-	switch (sconti) {
-		case '1':
-			scontoDaApplicare = scontoMinorenni
-			break
-		case '2':
-			scontoDaApplicare = scontoOver65
-			break
-		case '3':
-			scontoDaApplicare = 'nessuno conto'
-			break
-            document.getElementById('costo-biglietto').innerHTML += scontoDaApplicare
-	}
+    let prezzoFinale = prezzoBigliettoBase - sconti;
+    document.getElementById('costo-biglietto').innerHTML += prezzoFinale.toFixed(2);
 })
-
-
